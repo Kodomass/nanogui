6bcb755af3e4be4a2029924f31d54a5162465264
Kodomass <a.abzhanov@gmail.com>
2016-07-06 15:39:47 -0400

    Init console widget [#125285001]

---
+++ include/nanogui/console.h
@@ -0,0 +1,102 @@
+/*
+    nanogui/Console.h -- Fancy text box with builtin regular
+    expression-based validation
+
+    The text box widget was contributed by Christian Schueller.
+
+    NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
+    The widget drawing code is based on the NanoVG demo application
+    by Mikko Mononen.
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE.txt file.
+*/
+
+#pragma once
+
+#include <nanogui/compat.h>
+#include <nanogui/widget.h>
+#include <sstream>
+
+NAMESPACE_BEGIN(nanogui)
+
+class NANOGUI_EXPORT Console : public Widget {
+public:
+    enum class Alignment {
+        Left,
+        Center,
+        Right
+    };
+
+    Console(Widget *parent, const std::string &value = "Untitled");
+
+    bool editable() const { return mEditable; }
+    void setEditable(bool editable);
+
+    const std::string &value() const { return mValue; }
+    void setValue(const std::string &value) { mValue = value; }
+
+    const std::string &defaultValue() const { return mDefaultValue; }
+    void setDefaultValue(const std::string &defaultValue) { mDefaultValue = defaultValue; }
+
+    Alignment alignment() const { return mAlignment; }
+    void setAlignment(Alignment align) { mAlignment = align; }
+
+    /// Return the underlying regular expression specifying valid formats
+    const std::string &format() const { return mFormat; }
+    /// Specify a regular expression specifying valid formats
+    void setFormat(const std::string &format) { mFormat = format; }
+
+    /// Set the \ref Theme used to draw this widget
+    virtual void setTheme(Theme *theme) override;
+
+    /// Set the change callback
+    std::function<bool(const std::string& str)> callback() const { return mCallback; }
+    void setCallback(const std::function<bool(const std::string& str)> &callback) { mCallback = callback; }
+
+    virtual bool mouseButtonEvent(const Vector2i &p, int button, bool down, int modifiers) override;
+    virtual bool mouseMotionEvent(const Vector2i &p, const Vector2i &rel, int button, int modifiers) override;
+    virtual bool mouseDragEvent(const Vector2i &p, const Vector2i &rel, int button, int modifiers) override;
+    virtual bool mouseEnterEvent(const Vector2i &p, bool enter) override;
+    virtual bool focusEvent(bool focused) override;
+    virtual bool keyboardEvent(int key, int scancode, int action, int modifiers) override;
+    virtual bool keyboardCharacterEvent(unsigned int codepoint) override;
+
+    virtual Vector2i preferredSize(NVGcontext *ctx) const override;
+    virtual void draw(NVGcontext* ctx) override;
+    virtual void save(Serializer &s) const override;
+    virtual bool load(Serializer &s) override;
+protected:
+    bool checkFormat(const std::string& input,const std::string& format);
+    bool copySelection();
+    void pasteFromClipboard();
+    bool deleteSelection();
+
+    void updateCursor(NVGcontext *ctx, float lastx,
+                      const NVGglyphPosition *glyphs, int size);
+    float cursorIndex2Position(int index, float lastx,
+                               const NVGglyphPosition *glyphs, int size);
+    int position2CursorIndex(float posx, float lastx,
+                             const NVGglyphPosition *glyphs, int size);
+protected:
+    bool mEditable;
+    bool mCommitted;
+    std::string mValue;
+    std::string mDefaultValue;
+    Alignment mAlignment;
+    std::string mFormat;
+    std::function<bool(const std::string& str)> mCallback;
+    bool mValidFormat;
+    std::string mValueTemp;
+    int mCursorPos;
+    int mSelectionPos;
+    Vector2i mMousePos;
+    Vector2i mMouseDownPos;
+    Vector2i mMouseDragPos;
+    int mMouseDownModifier;
+    float mTextOffset;
+    double mLastClick;
+    int mRows;
+};
+
+NAMESPACE_END(nanogui)
---
+++ src/example3.cpp
@@ -0,0 +1,129 @@
+/*
+    src/example1.cpp -- C++ version of an example application that shows
+    how to use the various widget classes. For a Python implementation, see
+    '../python/example1.py'.
+
+    NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
+    The widget drawing code is based on the NanoVG demo application
+    by Mikko Mononen.
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE.txt file.
+*/
+
+#include <nanogui/screen.h>
+#include <nanogui/window.h>
+#include <nanogui/layout.h>
+#include <nanogui/label.h>
+#include <nanogui/checkbox.h>
+#include <nanogui/button.h>
+#include <nanogui/toolbutton.h>
+#include <nanogui/popupbutton.h>
+#include <nanogui/combobox.h>
+#include <nanogui/progressbar.h>
+#include <nanogui/entypo.h>
+#include <nanogui/messagedialog.h>
+#include <nanogui/textbox.h>
+#include <nanogui/slider.h>
+#include <nanogui/imagepanel.h>
+#include <nanogui/imageview.h>
+#include <nanogui/vscrollpanel.h>
+#include <nanogui/colorwheel.h>
+#include <nanogui/graph.h>
+#include <nanogui/tabwidget.h>
+#include <nanogui/console.h>
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+#include <nanogui/glutil.h>
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::to_string;
+
+class ExampleApplication : public nanogui::Screen {
+public:
+    ExampleApplication() : nanogui::Screen(Eigen::Vector2i(1024, 768), "NanoGUI Test") {
+        using namespace nanogui;
+
+        Window *window = new Window(this, "Button demo");
+        window->setPosition(Vector2i(15, 15));
+        window->setLayout(new GroupLayout());
+        mProgress = new ProgressBar(window);
+
+              
+        // Console example 
+        //
+        window = new Window(this, "Simple Console");
+        window->setPosition(Vector2i(145, 15));
+        window->setLayout(new GroupLayout());
+
+        Console *console = new Console(window);
+        console->setFixedSize(Vector2i(400, 200));
+        console->setValue("Some initial text \n with line break");
+        console->setFontSize(20);
+        console->setEditable(true);
+        console->setAlignment(Console::Alignment::Left);
+
+
+        performLayout();
+
+   }
+
+    ~ExampleApplication() {
+    }
+
+    virtual bool keyboardEvent(int key, int scancode, int action, int modifiers) {
+        if (Screen::keyboardEvent(key, scancode, action, modifiers))
+            return true;
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+            setVisible(false);
+            return true;
+        }
+        return false;
+    }
+
+    virtual void draw(NVGcontext *ctx) {
+        /* Animate the scrollbar */
+        mProgress->setValue(std::fmod((float) glfwGetTime() / 10, 1.0f));
+
+        /* Draw the user interface */
+        Screen::draw(ctx);
+    }
+
+    virtual void drawContents() {
+    }
+
+private:
+    nanogui::ProgressBar *mProgress;
+    nanogui::GLShader mShader;
+};
+
+int main(int /* argc */, char ** /* argv */) {
+    try {
+        nanogui::init();
+
+        {
+            nanogui::ref<ExampleApplication> app = new ExampleApplication();
+            app->drawAll();
+            app->setVisible(true);
+            nanogui::mainloop();
+        }
+
+        nanogui::shutdown();
+    } catch (const std::runtime_error &e) {
+        std::string error_msg = std::string("Caught a fatal error: ") + std::string(e.what());
+        #if defined(_WIN32)
+            MessageBoxA(nullptr, error_msg.c_str(), NULL, MB_ICONERROR | MB_OK);
+        #else
+            std::cerr << error_msg << endl;
+        #endif
+        return -1;
+    }
+
+    return 0;
+}
